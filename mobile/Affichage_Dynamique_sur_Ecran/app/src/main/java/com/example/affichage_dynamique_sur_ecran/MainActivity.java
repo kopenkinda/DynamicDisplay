dac1f8689b85a076d3a1c2295e604a7c25e110f4
AlexisSarlatYnov <alexis.sarlat@ynov.com>
2020-03-11 14:51:39 +0100

    initial commit mobile

---
+++ mobile/Affichage_Dynamique_sur_Ecran/app/src/main/java/com/example/affichage_dynamique_sur_ecran/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.affichage_dynamique_sur_ecran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
